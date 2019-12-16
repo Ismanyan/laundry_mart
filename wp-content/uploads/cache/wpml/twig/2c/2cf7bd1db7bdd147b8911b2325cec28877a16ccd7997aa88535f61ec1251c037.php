e75c908dd2f67a88e5338813033110ceb84bf8ee
Bilkis Ismail <bilkisismail07@gmail.com>
2019-12-16 19:27:18 +0700

    initial project

---
+++ wp-content/uploads/cache/wpml/twig/2c/2cf7bd1db7bdd147b8911b2325cec28877a16ccd7997aa88535f61ec1251c037.php
@@ -0,0 +1,64 @@
+<?php
+
+/* preview.twig */
+class __TwigTemplate_9a43afb10d3fa5d13fb983847d5b57ffe6dd8a2fdbfaa7c5fbcd9fccca1b183b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div class=\"js-wpml-ls-preview-wrapper wpml-ls-preview-wrapper";
+        if (($context["class"] ?? null)) {
+            echo " ";
+            echo twig_escape_filter($this->env, ($context["class"] ?? null), "html", null, true);
+        }
+        echo "\">
+    <strong class=\"wpml-ls-preview-label\">";
+        // line 2
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["strings"] ?? null), "misc", array()), "label_preview", array()), "html", null, true);
+        echo "</strong>
+    <span class=\"spinner\"></span>
+    <div class=\"js-wpml-ls-preview\">";
+        // line 4
+        echo $this->getAttribute(($context["preview"] ?? null), "html", array());
+        echo "</div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "preview.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  32 => 4,  27 => 2,  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "preview.twig", "E:\\xampp708\\htdocs\\wp\\korando\\wp-content\\plugins\\sitepress-multilingual-cms\\templates\\language-switcher-admin-ui\\preview.twig");
+    }
+}
